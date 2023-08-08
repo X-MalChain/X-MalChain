87366915bcc4c0e0972b744467bca2c4ae98410b
X-MalChain <141179257+X-MalChain@users.noreply.github.com>
2023-08-08 22:32:38 +0800

    Update models.py

--- common/models.py
+++ common/models.py
@@ -2,10 +2,8 @@ from django.db import models
 
 
 # Create your models here.
-# 存放数据表的定义
 
-
-# KG 表，存放节点信息
+# Basic Knowledge graph
 class KgTest(models.Model):
     nodeID = models.IntegerField()
     actionName = models.CharField(max_length=120)
@@ -13,16 +11,13 @@ class KgTest(models.Model):
     apiList = models.CharField(max_length=120, blank=True)
 
 
-# KG 表，存放节点信息
-# 更新后的节点数据表
+# After the first augmentation
 class KgBackup(models.Model):
     nodeID = models.IntegerField(null=True, blank=True)
     actionName = models.CharField(max_length=120)
     perList = models.CharField(max_length=120, blank=True, default='')
     apiList = models.CharField(max_length=120, blank=True, default='')
 
-
-# Api表，存放Api的基本信息，包括引入和弃用的API Level，和与之相关联的AddApiTest和RepApiTest
 class ApiTest(models.Model):
     apiID = models.IntegerField(blank=True)
     apiName = models.CharField(max_length=200)
@@ -33,39 +28,31 @@ class ApiTest(models.Model):
     # repList = models.IntegerField()
     repList = models.CharField(max_length=200, blank=True)
 
-
 class AddApiTest(models.Model):
     listID = models.IntegerField()
     list = models.CharField(max_length=600)
-
 
 class ApiSim(models.Model):
     listID = models.IntegerField(blank=True)
     list = models.CharField(max_length=600)
 
-
 class ApiSDK(models.Model):
     listID = models.IntegerField(blank=True)
     list = models.CharField(max_length=600)
-
 
 class PerSDK(models.Model):
     listID = models.IntegerField(blank=True)
     list = models.CharField(max_length=600)
 
-
 class RepApiTest(models.Model):
     listID = models.IntegerField()
     list = models.CharField(max_length=600)
 
-
-# Permission表，存放Permission的基本信息，包括引入和弃用的API Level
 class PerTest(models.Model):
     perID = models.IntegerField()
     perName = models.CharField(max_length=200)
     inLevel = models.IntegerField()
     outLevel = models.IntegerField()
-
 
 class relTest(models.Model):
     sourceID = models.IntegerField()
@@ -75,7 +62,7 @@ class relTest(models.Model):
     relation = models.CharField(max_length=120, blank=True)
 
 
-# 更新后的关系数据表
+# Renew
 class relBackup(models.Model):
     sourceID = models.IntegerField()
     sourceAct = models.CharField(max_length=200, blank=True, default='')
@@ -83,11 +70,9 @@ class relBackup(models.Model):
     targetAct = models.CharField(max_length=200, blank=True, default='')
     relation = models.CharField(max_length=120, blank=True)
 
-
 class sensitiveApi(models.Model):
     api = models.CharField(max_length=200, blank=True, default='')
     permission = models.CharField(max_length=200, blank=True, default='')
-
 
 class sensitiveApiMini(models.Model):
     api = models.CharField(max_length=200, blank=True, default='')
@@ -96,14 +81,12 @@ class sensitiveApiMini(models.Model):
     description = models.CharField(max_length=400, blank=True, default='')
     name = models.CharField(max_length=200, blank=True, default='')
 
-
-# 调用api所需要的申请的权限
 class apiRequsetPer(models.Model):
     api = models.CharField(max_length=250, blank=True, default='')
     per = models.CharField(max_length=250, blank=True, default='')
 
 
-# 测试图谱自动扩展，测试数据库
+# Test for upgrading
 class augmenTestNode(models.Model):
     nodeID = models.IntegerField(null=True, blank=True)
     actionName = models.CharField(max_length=120)
@@ -111,7 +94,6 @@ class augmenTestNode(models.Model):
     apiList = models.CharField(max_length=120, blank=True, default='')
     mark = models.CharField(max_length=50, blank=True, default='')
 
-# 测试扩展的节点关系数据表
 class augmenTestRel(models.Model):
     sourceID = models.IntegerField()
     sourceAct = models.CharField(max_length=200, blank=True, default='')
@@ -119,7 +101,6 @@ class augmenTestRel(models.Model):
     targetAct = models.CharField(max_length=200, blank=True, default='')
     relation = models.CharField(max_length=120, blank=True)
 
-# Permission表，用于测试扩展
 class augmenTestPer(models.Model):
     # perID = models.IntegerField(blank=True)
     perID = models.IntegerField(null=True, blank=True)
@@ -127,7 +108,6 @@ class augmenTestPer(models.Model):
     inLevel = models.CharField(max_length=50, blank=True)
     outLevel = models.CharField(max_length=50, blank=True)
 
-# Api表，用于测试扩展
 class augmenTestAPi(models.Model):
     # apiID = models.IntegerField(blank=True)
     apiID = models.IntegerField(null=True, blank=True)
@@ -138,8 +118,8 @@ class augmenTestAPi(models.Model):
     repList = models.CharField(max_length=200, blank=True)
 
 
-# 下面为备份扩充的知识图谱
-# 备份经过第一次数据集（amd）扩充后的知识图谱
+# Backup
+# the first round upgrading
 class augmentAMDNode(models.Model):
     nodeID = models.IntegerField(null=True, blank=True)
     actionName = models.CharField(max_length=120)
@@ -170,7 +150,7 @@ class augmentAMDAPi(models.Model):
     addList = models.CharField(max_length=200, blank=True)
     repList = models.CharField(max_length=200, blank=True)
 
-# 备份经过第2次数据集（amd+drebin）扩充后的知识图谱
+# the second upgrading
 class augmentNode2(models.Model):
     nodeID = models.IntegerField(null=True, blank=True)
     actionName = models.CharField(max_length=120)
@@ -192,7 +172,6 @@ class augmentPer2(models.Model):
     inLevel = models.CharField(max_length=50, blank=True)
     outLevel = models.CharField(max_length=50, blank=True)
 
-# Api表，用于测试扩展
 class augmentAPi2(models.Model):
     # apiID = models.IntegerField(blank=True)
     apiID = models.IntegerField(null=True, blank=True)
@@ -202,7 +181,7 @@ class augmentAPi2(models.Model):
     addList = models.CharField(max_length=200, blank=True)
     repList = models.CharField(max_length=200, blank=True)
 
-# 备份经过第3次数据集（amd+drebin）扩充后的知识图谱
+# The third upgrading
 class augmentNode3(models.Model):
     nodeID = models.IntegerField(null=True, blank=True)
     actionName = models.CharField(max_length=120)
@@ -224,7 +203,6 @@ class augmentPer3(models.Model):
     inLevel = models.CharField(max_length=50, blank=True)
     outLevel = models.CharField(max_length=50, blank=True)
 
-# Api表，用于测试扩展
 class augmentAPi3(models.Model):
     # apiID = models.IntegerField(blank=True)
     apiID = models.IntegerField(null=True, blank=True)
@@ -235,7 +213,7 @@ class augmentAPi3(models.Model):
     repList = models.CharField(max_length=200, blank=True)
 
 
-# 中间数据库
+# The inter-upgrading
 class augmentNodeIn(models.Model):
     nodeID = models.IntegerField(null=True, blank=True)
     actionName = models.CharField(max_length=120)
@@ -257,7 +235,6 @@ class augmentPerIn(models.Model):
     inLevel = models.CharField(max_length=50, blank=True)
     outLevel = models.CharField(max_length=50, blank=True)
 
-# Api表，用于测试扩展
 class augmentAPiIn(models.Model):
     # apiID = models.IntegerField(blank=True)
     apiID = models.IntegerField(null=True, blank=True)
